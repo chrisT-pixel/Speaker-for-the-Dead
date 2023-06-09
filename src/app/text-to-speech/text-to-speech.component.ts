daa2fb24470ec5e081f2c059aac828b39a7a2236
Chris Taylor <christophertaylor0811@gmail.com>
2023-06-09 11:03:39 +0930

    remove unused methods and improve comments

--- src/app/text-to-speech/text-to-speech.component.ts
+++ src/app/text-to-speech/text-to-speech.component.ts
@@ -37,8 +37,7 @@ export class TextToSpeechComponent implements OnInit {
 		this.text = this.responseString!;
 		this.sayCommand = "";
 
-		// These are "recommended" in so much as that these are the voices that I (Chris)
-		// could understand most clearly.
+		// Voices that speak clearly - can be useful if creating different characters that depend on this API
 		this.recommendedVoices = Object.create( null );
 		this.recommendedVoices[ "Alex" ] = true;
 		this.recommendedVoices[ "Alva" ] = true;
@@ -71,55 +70,33 @@ export class TextToSpeechComponent implements OnInit {
         this.myTimeout = setTimeout(this.resetSpeech, 14000);
     }
 
-	// I demo the currently-selected voice. Not currently in use in app
-	public demoSelectedVoice() : void {
-
-		if ( ! this.selectedVoice ) {
-			console.warn( "Expected a voice, but none was selected." );
-			return;
-		}
-
-		var demoText = "Best wishes and warmest regards.";
-
-		this.stop();
-		this.synthesizeSpeechFromText( this.selectedVoice, this.selectedRate, demoText );
-
-	}
-
-
-	// I get called once after the inputs have been bound for the first time.
+	// this is called only once after all inputs have been bound initially
 	public ngOnInit() : void {
 
 		this.detectResponseChanges();
 		this.voices = speechSynthesis.getVoices();
 		this.selectedVoice = ( this.voices[ 0 ] || null );
-		this.updateSayCommand();
-
-		// The voices aren't immediately available (or so it seems). As such, if no
-		// voices came back, let's assume they haven't loaded yet and we need to wait for
-		// the "voiceschanged" event to fire before we can access them.
+		
+		//if no voices are returned, assume they haven't loaded yet and waiting for
+		// the "voiceschanged" event to fire is necessary before we can access them.
 		if ( ! this.voices.length ) {
 
 			speechSynthesis.addEventListener(
 				"voiceschanged",
 				() => {
-
+					//TODO: note this returns all voices - would be more efficient to just get the voice we need
 					this.voices = speechSynthesis.getVoices();
 					this.selectedVoice = ( this.voices[ 0 ] || null );
-					this.updateSayCommand();
-
 				}
 			);
-
 		}
-
 	}
 
 
-	// I synthesize speech from the current text for the currently-selected voice.
+	// Synthesize speech from the current text for the currently-selected voice.
 	public speak() : void {
-		this.stop(); 
 		
+		this.stop(); 
 		//index of voices[51] is of UK male
 		this.synthesizeSpeechFromText( this.voices[51], this.selectedRate, this.responseString! );
 
@@ -130,45 +107,21 @@ export class TextToSpeechComponent implements OnInit {
 	public stop() : void {
 
 		if ( speechSynthesis.speaking ) {
-
 			speechSynthesis.cancel();
-
 		}
 
 	}
 
-	// Not currently used in app
-	// I update the "say" command that can be used to generate the a sound file from the
-	// current speech synthesis configuration.
-	public updateSayCommand() : void {
-		
-		if ( ! this.selectedVoice || ! this.text ) {
-			return;
-		}
-
-		// With the say command, the rate is the number of words-per-minute. As such, we
-		// have to finagle the SpeechSynthesis rate into something roughly equivalent for
-		// the terminal-based invocation.
-		var sanitizedRate = Math.floor( 200 * this.selectedRate );
-		var sanitizedText = this.text
-			.replace( /[\r\n]/g, " " )
-			.replace( /(["'\\\\/])/g, "\\$1" )
-		;
-
-		this.sayCommand = `say --voice ${ this.selectedVoice.name } --rate ${ sanitizedRate } --output-file=demo.aiff "${ sanitizedText }"`;
-
-	}
 
 	// ---
 	// PRIVATE METHODS.
 	// ---
 
-	// I perform the low-level speech synthesis for the given voice, rate, and text.
+	// Perform the low-level speech synthesis for the given voice, rate, and text.
 	private synthesizeSpeechFromText(voice: SpeechSynthesisVoice, rate: number, text: string) : void {
 		
 		var myTimeout: any;
 		var utterance = new SpeechSynthesisUtterance(this.responseString);
-		//utterance.voice = this.selectedVoice;
 		utterance.voice = this.voices[51]; //index 51 is of UK male
 		utterance.rate = rate;
 
